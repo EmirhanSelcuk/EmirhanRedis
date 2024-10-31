d5c84a78a7fd12106fc49722cec89c0e35f4ef67
emirhanselcuk <selcukemirhan88@gmail.com>
2024-10-31 15:10:23 +0300

    Add project files.

---
+++ EmirhanRedis.API/Model/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmirhanRedis.API.Model
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product() {Id=1, Name="Pen 1" },
+                new Product() {Id=2, Name="Pen 2" },
+                new Product() {Id=3, Name="Pen 3" }
+                );
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EmirhanRedis.API/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using EmirhanRedis.API.Model;
+
+namespace EmirhanRedis.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+    }
+}
